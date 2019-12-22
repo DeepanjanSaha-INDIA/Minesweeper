b6ca6fbb7bc548c65ce1630f354cc7ce9f1338c0
Deepanjan <deepsaha20@gmail.com>
2019-12-22 20:32:00 +0530

    Implemented timer

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  minutes: number = 0;
+  seconds: number = 0;
+  bombs: number = 10;
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => {
+      this.seconds += 1;
+      if(this.seconds == 60){
+        this.minutes += 1;
+        this.seconds = 0;
+      }
+    }, 1000);
   }
 
 }
